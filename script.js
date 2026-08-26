1b5c98d7c4d356a24d05ecba428729783b953b3b
SinNombre-vr <sinnombre.acbs@gmail.com>
2026-08-26 21:55:59 +0200

    fix: load optimized i18n v2

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
   "use strict";
 
   const scripts = [
-    { src: "i18n.js?v=1", attr: "data-toolhub-i18n" },
+    { src: "i18n-v2.js?v=2", attr: "data-toolhub-i18n" },
     { src: "script-main.js?v=24", attr: "data-toolhub-main" },
     { src: "herramientas/admin/toolhub-admin.js?v=1", attr: "data-toolhub-admin" }
   ];
