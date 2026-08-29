bd3a151cc21a25861384f076c59cbdf982bf5ee0
SinNombre-vr <sinnombre.acbs@gmail.com>
2026-08-29 16:08:40 +0200

    chore: invalidar cache de recientes extendidos

--- script.js
+++ script.js
@@ -11,7 +11,7 @@
     { src: "toolhub-tree-menu.js?v=2", attr: "data-toolhub-tree-menu" },
     { src: "toolhub-recent-assets.js?v=1", attr: "data-toolhub-recent-assets" },
     { src: "toolhub-recent-position-fix.js?v=2", attr: "data-toolhub-recent-position-fix" },
-    { src: "toolhub-recent-fill.js?v=1", attr: "data-toolhub-recent-fill" }
+    { src: "toolhub-recent-fill.js?v=2", attr: "data-toolhub-recent-fill" }
   ];
 
   function load(index) {
